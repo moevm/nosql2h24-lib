3da905f4ba694f9b2d451338a426672c784d2d5b
vgnlkn <vgn1lkn26@gmail.com>
2024-11-09 17:43:19 +0300

    tmp init app

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://gen_user:U3vU~xom)!O%2B%3B%26@147.45.189.115:27017/default_db?authSource=admin&directConnection=true')
-db = client['default_db']
-collection = db['books']
+client = MongoClient('mongodb://mongo:27017@0.0.0.0:27017')
+#print(client.list_database_names())
+# db = client['default_db']
+# collection = db['books']
+
+# @app.route('/add', methods=['POST'])
+# def add_item():
+#     #item = request.json
+#     #collection.insert_one(item)
+#     return jsonify({'message': 'Item added successfully!'}), 201
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    #items = list(collection.find({}, {'_id': 0}))
+    items = client.list_database_names()
+    return jsonify(items)
 
 if __name__ == '__main__':
     app.run(debug=True)
