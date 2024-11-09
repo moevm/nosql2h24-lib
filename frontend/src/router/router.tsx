d76c93fbeeb89fc3ea88977dad4601fe7da696bc
kyzinatra <danilsolom@mail.ru>
2024-11-09 19:47:01 +0300

    добавил главную страницу

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../components/home";
 import { Auth } from "../components/auth";
+import { Book } from "../components/book";
 
 export const router = createBrowserRouter([
 	{
@@ -14,5 +15,9 @@ export const router = createBrowserRouter([
 	{
 		path: "/registration",
 		element: <Home />,
+	},
+	{
+		path: "/book/:id",
+		element: <Book />,
 	},
 ]);
