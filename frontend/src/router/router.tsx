871fdda64a557966e50ad1ba28286f7fb2ee801c
kyzinatra <danilsolom@mail.ru>
2024-11-09 18:44:17 +0300

    добавил чакру

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Home } from "../components/home";
+import { Auth } from "../components/auth";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <div>Hello world!</div>,
+		element: <Home />,
+	},
+	{
+		path: "/auth",
+		element: <Auth />,
 	},
 ]);
