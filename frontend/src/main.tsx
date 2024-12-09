94079494d8e66c36815d1abe28df30527987125f
kyzinatra <danilsolom@mail.ru>
2024-12-09 12:39:46 +0300

    Добавляет тэг

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { router } from "./router/router.tsx";
 import { RouterProvider } from "react-router";
 import { Provider } from "./components/ui/provider.tsx";
 import "./style.css";
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Provider>
